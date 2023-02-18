0cefc326b9932347c12155124bec51f2629446b2
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-02-19 02:02:13 +0530

    Examples of 'string' data type
    Here we can find the different examples of 'string' data type in javascript

---
+++ 2_Datatypes/Strings.js
@@ -0,0 +1,21 @@
+// three ways of representing strings in JavaScript.
+
+// 1. Single Quotes ('string')
+
+const singleQuoteString = 'This is a single quote string'; // here how we can represent the strings in single quotes
+
+// 2. Double Quotes ("string") 
+
+const doubleQuoteString = "This is a double quote string"; // here how we can represent the strings in double quotes
+
+// 3. Backticks, or Template Literals ( string )
+
+const name = "Siddharth";
+const templateLiteralString = `My name is ${name}`; // here how we can represent the strings in Backticks
+
+// Template literals can also span multiple lines, which can make code easier to read and write.
+
+const multiLineString = `
+  This is a multi-line string
+  that spans multiple lines.
+  `;

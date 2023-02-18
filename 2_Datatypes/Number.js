38ffda2c11d9cab5495909ca67f5c10a7f9b9349
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-02-19 01:49:07 +0530

    Different examples of 'number' data type
    This file contains some examples of 'number' data types in javascript

---
+++ 2_Datatypes/Number.js
@@ -0,0 +1,15 @@
+// Here are some examples of number data types in JavaScript:
+const integer = 42;
+
+const floatingPoint = 3.14;
+
+const negativeNumber = -10;
+
+// Here are some examples of arithmetic operations with number data types in JavaScript: (+, -, *, /)
+const sum = 2 + 2; // 4
+
+const difference = 5 - 3; // 2
+
+const product = 4 * 6; // 24
+
+const quotient = 10 / 2; // 5

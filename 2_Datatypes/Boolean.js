9019656f2198144b7a73586ae64fbeef9dac244a
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-02-19 10:38:06 +0530

    Examples of boolean data type in JavaScript

--- 2_Datatypes/Boolean.js
+++ 2_Datatypes/Boolean.js
@@ -1,1 +1,17 @@
+// The boolean data type can only have two possible values:
+// 1. true
+// 2. false
 
+//Here are some examples of boolean data types in JavaScript:
+const isRaining = true;
+
+const isSunny = false;
+
+
+// The boolean data type is often used in conditional statements to control the flow of a program.
+
+const isRaining = true;
+
+if (isRaining) {
+  console.log("Don't forget to bring an umbrella!");
+}

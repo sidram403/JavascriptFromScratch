5b54c574831afc5bb0b27df207668e663db549dc
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-02-19 10:51:30 +0530

    Examples of 'objects' data type in JavaScript

--- 2_Datatypes/Objects.js
+++ 2_Datatypes/Objects.js
@@ -1,1 +1,11 @@
+// In JavaScript, an object is a complex data type that represents a collection of related data or functionality.
 
+// Here's an example of an object in JavaScript:
+
+const person = {firstName: 'John', lastName: 'Doe', age: 30, isStudent: true,}
+
+// Here's an example of accessing the firstName property of the person object using dot notation:
+console.log(person.firstName); // output: "John"
+
+// And here's an example of accessing the firstName property of the person object using bracket notation:
+console.log(person['firstName']); // output: "John"

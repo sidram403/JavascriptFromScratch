c1f5f9bd47ef57c51b93c4d7a1ba83e32f587358
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-02-19 12:27:01 +0530

    Examples about functions in Javascript

---
+++ 3_Functions/Functions.js
@@ -0,0 +1,29 @@
+// Function declarations
+function myFunction() {
+  // function body
+}
+
+// Function expressions
+const myFunction = function() {
+  // function body
+};
+
+// Arrow functions
+const myFunction = (param1, param2) => {
+  // function body
+};
+
+// Method functions
+const myObject = {
+  myFunction: function() {
+    // function body
+  }
+};
+
+// Here is an example of a basic function in JavaScript:
+function addNumbers(a, b) {
+  
+  return a + b;    // This function takes two parameters, a and b, adds them together, and returns the result.
+  
+}
+

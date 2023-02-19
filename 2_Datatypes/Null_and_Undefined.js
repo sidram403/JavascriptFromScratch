169a09acba1e1255c7c0e0d6d97cf1064ce34a82
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-02-19 10:44:46 +0530

    Examples of 'null and undefined' data type in JS

--- 2_Datatypes/Null_and_Undefined.js
+++ 2_Datatypes/Null_and_Undefined.js
@@ -1,1 +1,18 @@
+// In JavaScript, 'null' is a primitive data type that represents the intentional absence of any object value.
 
+// Here's an example of using the null value in JavaScript:
+let myVar = null;  // the variable myVar is assigned the null value, which indicates that it has no object value.
+
+console.log(myVar); // output: null
+
+// In JavaScript, 'undefined' is a special value that is used to represent an uninitialized or missing value.
+
+// Here's an example of using the undefined value in JavaScript:
+
+let myVar;  // the variable myVar has been declared but not assigned a value, so it has the value undefined.
+
+console.log(myVar); // output: undefined
+
+let myVar = undefined;
+
+console.log(myVar); // output: undefined

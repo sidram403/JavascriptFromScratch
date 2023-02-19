87bd482d68901cd37d40d6c014ba57b3a8a9787e
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-02-19 12:07:28 +0530

    updated examples objects data type

--- 2_Datatypes/Objects.js
+++ 2_Datatypes/Objects.js
@@ -2,10 +2,10 @@
 
 // Here's an example of an object in JavaScript:
 
-const person = {firstName: 'John', lastName: 'Doe', age: 30, isStudent: true,}
+const person = {firstName: 'Siddharth', lastName: 'Patil', age: 30, isStudent: true,}
 
 // Here's an example of accessing the firstName property of the person object using dot notation:
-console.log(person.firstName); // output: "John"
+console.log(person.firstName); // output: "Siddharth"
 
 // And here's an example of accessing the firstName property of the person object using bracket notation:
-console.log(person['firstName']); // output: "John"
+console.log(person['firstName']); // output: "Siddharth"

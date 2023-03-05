d556b7fba3247a4c1f72e835de50a6248bde75d3
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-03-05 10:51:44 +0530

    Detailed Explanation about Loops with example in JS

---
+++ 5_Loops/Loops.js
@@ -0,0 +1,27 @@
+// Here is an example of a for loop that counts from 0 to 4:
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// The output of this code will be:
+
+0
+1
+2
+3
+4
+
+let n=0
+while(n<5){
+  console.log(n)
+  n++
+}
+
+// the output of this code will be
+0
+1
+2
+3
+4
+
+  
